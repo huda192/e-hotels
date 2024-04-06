b5b4417c8303d2591dce37ef1623f3f3a63eb9c7
huda192 <107425665+huda192@users.noreply.github.com>
2024-04-05 20:50:10 -0400

    Update populate_db.sql

--- populate_db.sql
+++ populate_db.sql
@@ -579,16 +579,16 @@ values
     (212, 'yes'),
     (234, 'yes');
 
-insert into booking(roomid, isarchived, isrenting, custid)
-values 
-    (100,'no','yes', 5793),
-    (125,'yes','no',4148 ),
-    (258,'yes','no',1304),
-    (222,'yes','no',4320),
-    (130,'yes','no',0184),
-    (166,'no','yes',5493),
-    (161,'yes','no',4067),
-    (179,'yes','no',2940);
+insert into booking(roomid, isarchived, payment, isrenting, custid)
+values (100,'no',100,'yes', 5793),
+(125,'yes','no',150,4148 ),
+(258,'yes','no',210,1304),
+(222,'yes','no',,4320),
+(130,'yes','no',130,0184),
+(166,'no','yes',300,5493),
+(161,'yes','no',200,4067),
+(179,'yes','no',200,2940)
+
 
 insert into manager(empid, hoteladdress, hotelname)
 values
